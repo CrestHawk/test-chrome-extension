f4f93417f64f4d139f024e9de8f54276785f9e5f
Tom <emailtomdavies@gmail.com>
2019-11-03 18:11:37 +0000

    Testing calling to a local api

--- popup.js
+++ popup.js
@@ -5,11 +5,24 @@ chrome.storage.sync.get('color', function(data) {
     changeColor.setAttribute('value', data.color);
 });
 
+// changeColor.onclick = function(element) {
+//     let color = element.target.value;
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       chrome.tabs.executeScript(
+//           tabs[0].id,
+//           {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//     });
+//   };
+
 changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-  };
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://localhost:8064/users", true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      var resp = JSON.parse(xhr.responseText);
+
+      alert("User: " + resp.name + "'s id is " + resp.id + "!?");
+    }
+  }
+  xhr.send();
+};
